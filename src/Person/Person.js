bcedd8c924848ccfd250f897c9f4d76a3d0f5179
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2020-02-04 18:53:18 +0100

    added some code

---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const person = props => {
+    return (
+        <div>
+            <h2>I am very good person and I am {props.age} years</h2>
+        </div>
+    )
+}
+
+export default person;
