c1346f7f8a1bd29abbb271ae7546f359e4ef9846
Tomasz Duda <tduda@internal.stepstonesolutions.local>
2020-02-12 16:39:43 +0100

    added some features to code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import React from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <Person age={13}/>
-      <Person age={25}/>
-      <Person age={63}/>
-      <Person age={41}/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    people: [
+        {name: "janusz", age: 16},
+        {name: "anna", age: 20},
+        {name: "magda", age: 60},
+        {name: "kajetan", age: 6}
+    ]
+  };
+  onClickHandler = addTo => {
+      const copiedState = this.state.people.map(person => {
+          let {name, age} = person;
+          if(addTo === name){
+              age+=10;
+          };
+          return {name, age};
+      })
+      this.setState({people: copiedState})
+  };
+
+  showState = () => {
+    console.log(this.state.people);
+  };
+
+  render(){
+      const people = this.state.people.map(person => {
+          const {name, age} = person;
+          return (
+              <Person name={name} age={age} key={name} clicker={() => this.onClickHandler(name)}>
+                  {name === 'janusz' ? "I like video games!" : null}
+              </Person>
+          )
+      });
+      return (
+          <div className="App">
+              {people}
+              <button onClick={this.showState}>SHOW STATE</button>
+          </div>
+      );
+  }
+
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,9 @@ import React from 'react'
 const person = props => {
     return (
         <div>
-            <h2>I am very good person and I am {props.age} years</h2>
+            <h2>I am {props.name} person and I am {props.age} years</h2>
+            <p>{props.children}</p>
+            <button onClick={props.clicker}>Clicker</button>
         </div>
     )
 }
